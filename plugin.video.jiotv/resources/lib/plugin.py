fcfd1a11d4535ea59154c62951be6284e3ef6b6c
botallen <botallen1337@gmail.com>
2020-03-17 23:09:32 +0530

    JioTV updated + fixed #3

--- plugin.video.jiotv/resources/lib/plugin.py
+++ plugin.video.jiotv/resources/lib/plugin.py
@@ -8,9 +8,12 @@ from xbmcaddon import Addon
 from xbmc import executebuiltin, translatePath
 import os
 import sys
-from time import sleep
+from time import sleep, time
+from urllib import urlencode
+from datetime import datetime, date, timedelta
 from resources.lib import kodiutils
 from resources.lib import kodilogging
+import inputstreamhelper
 from xbmcgui import ListItem, Dialog
 from xbmcplugin import addDirectoryItems, endOfDirectory, setResolvedUrl
 from utils import check_login, _hotstarauth_key
@@ -31,8 +34,16 @@ def index():
         categories = json.load(f)
     items = []
     for each in categories.keys():
+        list_item = ListItem(
+            label=each + '  ({})'.format(len(categories[each])))
+        icon_url = "special://home/addons/plugin.video.jiotv/resources/extra/img/{}.png".format(
+            each)
+        list_item.setArt({
+            'thumb': icon_url,
+            'icon': icon_url
+        })
         items.append(
-            (plugin.url_for(show_category, each), ListItem(each), True))
+            (plugin.url_for(show_category, each), list_item, True))
     addDirectoryItems(plugin.handle, items)
     endOfDirectory(plugin.handle)
 
@@ -52,52 +63,153 @@ def show_category(category_id):
             'thumb': img_url,
             'icon': img_url
         })
-        list_item.setInfo('video', {
-            'title': channels[str(each)]['name'],
-            'genre': category_id,
-            'plot': '',
-            'mediatype': 'video'
+        items.append(
+            (plugin.url_for(show_epg, day=0, channel_id=each, live_url=channels[str(each)]['url']), list_item, True))
+
+    addDirectoryItems(plugin.handle, items)
+    endOfDirectory(plugin.handle)
+
+
+@plugin.route('/channel/<day>/<channel_id>')
+def show_epg(day, channel_id):
+    resp = requests.get(
+        "http://jiotv.data.cdn.jio.com/apis/v1.3/getepg/get?offset={0}&channel_id={1}&langId=6".format(day, channel_id)).json()
+    epg = sorted(
+        resp['epg'], key=lambda show: show['startEpoch'], reverse=True)
+    livetext = '[COLOR red][B][ LIVE ][/B][/COLOR]'
+    items = []
+    for each in epg:
+        current_epoch = int(time()*1000)
+        if not each['stbCatchupAvailable'] or each['startEpoch'] > current_epoch:
+            continue
+        islive = each['startEpoch'] < current_epoch and each['endEpoch'] > current_epoch
+        showtime = '   '+livetext if islive else datetime.fromtimestamp(
+            int(each['startEpoch']*.001)).strftime('    [ %I:%M %p -') + datetime.fromtimestamp(int(each['endEpoch']*.001)).strftime(' %I:%M %p ]   %a')
+        play_item = ListItem(
+            label=each['showname'] + showtime)
+        play_item.setArt({
+            'thumb': "http://jiotv.catchup.cdn.jio.com/dare_images/shows/"+each['episodePoster'],
+            'icon': "http://jiotv.catchup.cdn.jio.com/dare_images/shows/"+each['episodePoster'],
+            'fanart': "http://jiotv.catchup.cdn.jio.com/dare_images/shows/"+each['episodePoster'],
         })
-        list_item.setProperty('IsPlayable', 'true')
-        items.append((channels[str(each)]['url'], list_item, False))
+        play_item.setInfo('video', {
+            'title': each['showname'],
+            'genre': each['showGenre'],
+            'plot': each['episode_desc'],
+            'episode': 0 if each['episode_num'] == -1 else each['episode_num'],
+            'cast': each['starCast'].split(', '),
+            'director': each['director'],
+            'duration': each['duration']*60,
+            'tag': each['keywords'],
+            'mediatype': 'tvshow' if each['episode_num'] == -1 else 'episode',
+        })
+        play_item.setProperty('IsPlayable', 'true')
+        url = plugin.args.get('live_url')[0] if islive else plugin.url_for(
+            playcatchup, channel_name=resp['logoUrl'][:-4], startEpoch=each['startEpoch'])
+        items.append(
+            (url, play_item, False))
+    if len(items) == 0:
+        live_item = ListItem(livetext)
+        live_item.setArt({
+            'thumb': "special://home/addons/plugin.video.jiotv/resources/extra/img/live.png",
+            'icon': "special://home/addons/plugin.video.jiotv/resources/extra/img/live.png",
+        })
+        live_item.setInfo('video', {'title': livetext, 'mediatype': 'video'})
+        live_item.setProperty('IsPlayable', 'true')
+        items = [
+            (plugin.args.get('live_url')[0], live_item, False),
+            ('', ListItem('Catchup Not Available'), False)
+        ]
+    elif day == '0':
+        for i in range(-1, -7, -1):
+            label = 'Yesterday' if i == - \
+                1 else (date.today() + timedelta(days=i)).strftime('%A %d %B')
+            items.append(
+                (plugin.url_for(show_epg, day=i, channel_id=channel_id), ListItem(label), True))
+
     addDirectoryItems(plugin.handle, items)
     endOfDirectory(plugin.handle)
 
 
 @plugin.route('/play/<channel_name>')
 def play(channel_name):
-    play_item = ListItem()
-    hls = '' if not 'hls' in plugin.args else "?hls="+plugin.args['hls'][0]
-    url = "http://127.0.0.1:48996/play/{0}/master.m3u8{1}".format(
-        channel_name, hls)
-    if not url:
-        return
-    play_item.setPath(url)
-    play_item.setProperty('inputstreamaddon', 'inputstream.adaptive')
-    play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
-    play_item.setMimeType('application/vnd.apple.mpegurl')
-    play_item.setContentLookup(False)
+    is_helper = inputstreamhelper.Helper('hls')
+    if is_helper.check_inputstream():
+        play_item = ListItem()
+        hls = '' if not 'hls' in plugin.args else "?hls="+plugin.args['hls'][0]
+        url = "http://127.0.0.1:48996/play/{0}/master.m3u8{1}".format(
+            channel_name, hls)
+        if not url:
+            return
+        play_item.setPath(url)
+        play_item.setProperty('inputstreamaddon', is_helper.inputstream_addon)
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        play_item.setMimeType('application/vnd.apple.mpegurl')
+        play_item.setContentLookup(False)
 
-    setResolvedUrl(plugin.handle, True, listitem=play_item)
+        setResolvedUrl(plugin.handle, True, listitem=play_item)
 
 
 @plugin.route('/playstar/<channel_name>')
 def playstar(channel_name):
-    play_item = ListItem()
-    _auth = _hotstarauth_key()
-    url = "http://hotstar.live.cdn.jio.com/hotstar_isl/{0}/master.m3u8?hdnea={1}".format(
-        channel_name, _auth)
-    if not url:
-        return
-    play_item.setPath(url)
-    play_item.setProperty('inputstreamaddon', 'inputstream.adaptive')
-    play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
-    play_item.setProperty(
-        'inputstream.adaptive.stream_headers', "User-Agent=hotstar")
-    play_item.setMimeType('application/vnd.apple.mpegurl')
-    play_item.setContentLookup(False)
+    is_helper = inputstreamhelper.Helper('hls')
+    if is_helper.check_inputstream():
+        play_item = ListItem()
+        _auth = _hotstarauth_key()
+        url = "http://hotstar.live.cdn.jio.com/hotstar_isl/{0}/master.m3u8?hdnea={1}".format(
+            channel_name, _auth)
+        if not url:
+            return
+        play_item.setPath(url)
+        play_item.setProperty('inputstreamaddon', is_helper.inputstream_addon)
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        play_item.setProperty(
+            'inputstream.adaptive.stream_headers', "User-Agent=hotstar")
+        play_item.setMimeType('application/vnd.apple.mpegurl')
+        play_item.setContentLookup(False)
 
-    setResolvedUrl(plugin.handle, True, listitem=play_item)
+        setResolvedUrl(plugin.handle, True, listitem=play_item)
+
+
+@plugin.route('/playdash/<channel_name>')
+def playdash(channel_name):
+    is_helper = inputstreamhelper.Helper('mpd', drm='com.widevine.alpha')
+    if is_helper.check_inputstream():
+        play_item = ListItem()
+        _auth = _hotstarauth_key()
+        url = "http://hotstar.live.cdn.jio.com/hotstar_isl/{0}/master.mpd?hdnea={1}".format(
+            channel_name, _auth)
+        play_item.setPath(url)
+        play_item.setProperty('inputstreamaddon', is_helper.inputstream_addon)
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'mpd')
+        play_item.setProperty(
+            'inputstream.adaptive.license_type', 'com.widevine.alpha')
+        play_item.setProperty('inputstream.adaptive.license_key',
+                              'https://ipl.service.expressplay.com/hms/wv/rights/?ExpressPlayToken=BQAAABNlKfMAAAAAAGB5RXIUhuAKhb0o_gG4s6_qdxw4y5xQZyNGjvsbfiltjdLAStqy3hyJnAzQPRNmTknPc1nMTsezyHAxVCdu2VYmI-bCaJTYMefMpfs-fql1lF_B7Zrj-qyxdlafY1xKq42c6z1i9s1FPsE_z8wV6FC8BHNpMw&req_id=2f652cd6|User-Agent=hotstar&Content-Type=application/octet-stream|R{SSM}|')
+        play_item.setProperty(
+            'inputstream.adaptive.stream_headers', "User-Agent=hotstar")
+        play_item.setMimeType('application/dash+xml')
+        play_item.setContentLookup(False)
+
+        setResolvedUrl(plugin.handle, True, listitem=play_item)
+
+
+@plugin.route('/playcatchup/<channel_name>/<startEpoch>')
+def playcatchup(channel_name, startEpoch):
+    is_helper = inputstreamhelper.Helper('hls')
+    if is_helper.check_inputstream():
+        play_item = ListItem()
+        url = "http://127.0.0.1:48996/catchup/{0}/master.m3u8?startEpoch={1}".format(
+            channel_name, startEpoch)
+        if not url:
+            return
+        play_item.setPath(url)
+        play_item.setProperty('inputstreamaddon', is_helper.inputstream_addon)
+        play_item.setProperty('inputstream.adaptive.manifest_type', 'hls')
+        play_item.setMimeType('application/vnd.apple.mpegurl')
+        play_item.setContentLookup(False)
+
+        setResolvedUrl(plugin.handle, True, listitem=play_item)
 
 
 @plugin.route('/logout')
--- plugin.video.jiotv/service.py
+++ plugin.video.jiotv/service.py
@@ -3,6 +3,7 @@ import SocketServer
 import threading
 from resources.lib import kodiutils
 
+SocketServer.ThreadingTCPServer.allow_reuse_address = True
 handler = SocketServer.ThreadingTCPServer(("", 48996), proxy.JioTVProxy)
 t = threading.Thread(target=handler.serve_forever)
 t.setDaemon(True)
